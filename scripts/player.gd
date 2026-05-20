7cdf1326af7b1c3b6cdfb70424cc1be4d6d91928
moaazkamel <moaaz.kamel.gber@gmail.com>
2026-05-20 15:49:15 +0300

    Making large ship for decoration and rock wall. Making 5 boats for the player.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,50 +1,18 @@
 extends CharacterBody3D
 
-
-@export var acceleratrion: float = 10.0
-
-@export var max_speed: float = 40.0
-
-
-
-
+@export var speed: float = 20.0
 @export var turn_speed: float = 1.2
-@export var drag: float = 0.2
+@export var drift_grip: float = 2.0
 
-var Velocity: Vector3 = Vector3.ZERO
-var direction: Vector3 = Vector3.FORWARD
+func _physics_process(delta: float) -> void:
+	var turn = Input.get_axis("ui_right", "ui_left")
+	rotate_y(turn * turn_speed * delta)
 
+	var move = Input.get_axis("ui_up", "ui_down")
+	var direction = -transform.basis.z
 
-
-func _process(delta: float) -> void:
-	if Input.is_action_pressed('ui_down'):
-		
-		Velocity += direction * acceleratrion *delta
-		
-		
-	elif Input.is_action_pressed('ui_up'):
-		Velocity -= direction * acceleratrion *delta
+	var desired_velocity = direction * (move * speed)
 	
-	if Input.is_action_pressed('ui_left'):
-		
-		
-		rotate_y(turn_speed * delta)
-	elif Input.is_action_pressed('ui_right'):
-		
-		
-		rotate_y(-turn_speed * delta)
+	velocity = velocity.lerp(desired_velocity, drift_grip * delta)
 
-	Velocity = Velocity.lerp(Vector3.ZERO, drag *delta)
-	
-	
-	
-	if Velocity.length() > max_speed:
-		Velocity = Velocity.normalized() * max_speed
-		
-		
-		
-	translate(Velocity * delta)
-		
-		
-		
-	direction = -transform.basis.z
+	move_and_slide()
